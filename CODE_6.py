6b878ebe61de72d6ffb3d84037eb1786c385cca0
PavaniVedantham <136493353+PavaniVedantham@users.noreply.github.com>
2023-06-13 22:52:50 -0400

    CODE_6.py

--- CODE_6.py
+++ CODE_6.py
@@ -1,16 +1,19 @@
 def main():
 
-    filename = input("Enter the filename: ")
+    file_name = input('Enter input filename: ')
 
     try:
-        with open(filename, "r") as file:
-            print("Name\t\tHours Worked\t\tWages Paid")
-            for line in file:
+        with open(file_name, "r") as file:
+            print('{:<12s} {:>10s} {:>10s}'.format('Name', 'Hours Worked', 'Wages Paid'))
+            for line in file: # to calculate the value
                 name, hourly_wage, hours_worked = line.strip().split(" ")
                 wages_paid = float(hourly_wage) * float(hours_worked)
-                print(f"{name}\t\t{hours_worked}\t\t${wages_paid:.2f}")
+                print('{:<12s} {:>10d} {:>10.2f}'.format(name, hours_worked, wages_paid))
     except FileNotFoundError:
-        print("Error: Could not read file")
+        print("Error: File not found")
+    except ValueError:
+        print("Error: Invalid file format")
+
 
         
 if __name__ == "__main__":
