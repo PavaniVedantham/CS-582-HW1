f8c21ff114ad31f0c7726a4257143412a940bf73
PavaniVedantham <136493353+PavaniVedantham@users.noreply.github.com>
2023-06-13 23:06:40 -0400

    CODE_7.py

--- CODE_7.py
+++ CODE_7.py
@@ -1,23 +1,10 @@
-import statistics as stats
-import pandas as pd
+import statistics 
 
 def main():
     num = [8,3,4,2,6,7,9]
-
-    
-    mean_value = stats.mean(num)
-    print("Mean value is:", mean_value)
-
-    
-    median_value = stats.median(num)
-    print("Median value is:", median_value)
-
-    
-    mode_values = stats.mode(num)
-    if mode_values is None:
-        print("No mode exists.")
-    else:
-        print(f"Mode value is:{mode_values}")
+    print("Mean value is:", statistics.mode(num))
+    print("Median value is:", statistics.median(num))
+    print(f"Mode value is:", statistics.mean(num))
 
 
 if __name__ == "__main__":
